fd1b0a12a5fa9b490c052cf17bd1b833ec3910c3
Ameymore123 <142572157+Ameymore123@users.noreply.github.com>
2025-07-28 13:12:28 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -181,18 +181,26 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
   // Disable right-click
-  document.addEventListener('contextmenu', event => event.preventDefault());
+document.addEventListener('contextmenu', event => event.preventDefault());
 
-  // Disable certain key combos
-  document.onkeydown = function(e) {
-    if (
-      e.keyCode == 123 || // F12
-      (e.ctrlKey && e.shiftKey && (e.keyCode == 73 || e.keyCode == 74)) || // Ctrl+Shift+I or J
-      (e.ctrlKey && e.keyCode == 85) // Ctrl+U
+
+
+document.onkeydown = function(e) {
+    if (e.keyCode == 123 || // F12
+        (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) || // Ctrl+Shift+I
+        (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) || // Ctrl+Shift+C
+        (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) || // Ctrl+Shift+J
+        (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) // Ctrl+U
     ) {
       return false;
     }
   };
+
+
+
+
+
+
 
 
 
